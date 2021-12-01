4621e89aaae67df7ad0ddac42715836b7855865a
Erwann Labrot <65678624+Erwann-Lt@users.noreply.github.com>
2021-12-01 14:41:43 +0100

    Update Groupe.js

--- src/groupe/Groupe.js
+++ src/groupe/Groupe.js
@@ -4,15 +4,85 @@ import Groupe3 from './Groupe3'
 import Groupe4 from './Groupe4'
 import Groupe5 from './Groupe5'
 import Groupe6 from './Groupe6'
-
+import useWindowSize from '../useWindowSize'
+import HomeMiddle4 from '../home/HomeMiddle4'
 
 const Groupe = () => {
     const myRef = useRef(null)
  
-
+    const {width}= useWindowSize()
     const executeScroll = () => myRef.current.scrollIntoView()   
 
     return (
+        <div>
+        {width <650 ?
+         <div className='container-fluid'>
+        <br/><br/><br/><br/><br/><br/>
+       
+<div className="gridr1 "  >
+     <div
+    style={{
+    backgroundSize:'50%', 
+    backgroundRepeat:'no-repeat',
+    backgroundPositionX:'100%',
+    boxShadow:'500px'
+    }} 
+    className="groupe-pic1 ">
+
+   <h1
+   style={{ 
+   fontWeight:'bold', 
+   color:'#54565a',
+   marginLeft:'100px'
+    }}> 
+    Un groupe dédié<br/>
+    aux métiers de <br/>
+    <em className='highlight'>
+    l’énergie</em> et du conseil</h1>
+     
+   </div>
+</div>
+
+
+<div className='gridr4'>
+
+<div onClick={executeScroll}
+     style={{cursor:'pointer'}} 
+     className='d-flex  '>
+        <img height='5%'
+         src='https://uploads-ssl.webflow.com/5f0c8ee786502d713c4bbd11/5f47887bb72e4752207e03b6_Arrow.svg' alt=''  />
+          &nbsp;
+        <p >En savoir plus</p>
+</div>
+
+ 
+
+<div>
+    <p style={{color:'gray'}}>
+    Enalia rassemble et connecte des entreprises engagées dans l'efficacité énergétique.</p>
+</div>
+
+<div>
+ <span style={{color:'gray'}} className='highlight'>
+Toutes partagent les mêmes valeurs et la même ambition.</span>  
+</div>
+</div>
+
+
+
+<div ref={myRef}  >
+<br/> <br/><br/>
+  
+</div>
+
+ 
+
+ 
+<Groupe5 />
+  <HomeMiddle4 />
+        </div>
+       
+        :
         <div className='container-fluid'>
         <br/><br/><br/><br/><br/><br/>
        
@@ -78,6 +148,10 @@ Toutes partagent les mêmes valeurs et la même ambition.</span>
 <Groupe5 />
 <Groupe6 />
         </div>
+
+        
+        }
+       </div>
     )
 }
 
